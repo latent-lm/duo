aec22ba0a61f2b05a4688f1a85ce09cec897fd77
FrankCCCCC <unaxultraspaceos5@gmail.com>
2026-05-06 14:24:37 -0400

    add exp script for unigram test

--- unigram_test_script/unigram_test_optim.sh
+++ unigram_test_script/unigram_test_optim.sh
@@ -4,15 +4,84 @@ set -euo pipefail
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$ROOT_DIR"
 
+# python unigram/unigram_test2_polar_optim.py -m \
+#   hydra.job.chdir=true \
+#   hydra.sweep.dir=outputs/unigram_test2 \
+#   'hydra.sweep.subdir=unigram_test2_losses_sp${max_steps}_lr${lr}_pt${proposal_type}_per${proposal_exp_rate}_ht${hyper_T}_vs${vocab_size}_polar_optim' \
+#   +folder=artifacts \
+#   +vocab_size=2 \
+#   +max_steps=20000 \
+#   +lr=1e-5 \
+#   '+ps=[0.8,0.2]' \
+#   +proposal_type=unif \
+#   +proposal_exp_rate=1.0 \
+#   +hidden_size=128 \
+#   +depth=3 \
+#   +hyper_T=1000 \
+#   +hyper_dt=0.01 \
+#   +loss_plot_ma_window=50
+
+# python unigram/unigram_test2_polar_optim.py -m \
+#   hydra.job.chdir=true \
+#   hydra.sweep.dir=outputs/unigram_test2 \
+#   'hydra.sweep.subdir=unigram_test2_losses_sp${max_steps}_lr${lr}_pt${proposal_type}_per${proposal_exp_rate}_ht${hyper_T}_vs${vocab_size}_polar_optim' \
+#   +folder=artifacts \
+#   +vocab_size=2 \
+#   +max_steps=20000 \
+#   +lr=1e-5 \
+#   '+ps=[0.8,0.2]' \
+#   +proposal_type=exp \
+#   +proposal_exp_rate=0.01,0.1,0.5,1.0,2.0,3.0 \
+#   +hidden_size=128 \
+#   +depth=3 \
+#   +hyper_T=1e12 \
+#   +hyper_dt=0.01 \
+#   +loss_plot_ma_window=50
+
+# python unigram/unigram_test2_cart_optim.py -m \
+#   hydra.job.chdir=true \
+#   hydra.sweep.dir=outputs/unigram_test2 \
+#   'hydra.sweep.subdir=unigram_test2_losses_sp${max_steps}_lr${lr}_pt${proposal_type}_per${proposal_exp_rate}_ht${hyper_T}_vs${vocab_size}_cart_optim' \
+#   +folder=artifacts \
+#   +vocab_size=2 \
+#   +max_steps=20000 \
+#   +lr=1e-5 \
+#   '+ps=[0.8,0.2]' \
+#   +proposal_type=unif \
+#   +proposal_exp_rate=1.0 \
+#   +hidden_size=128 \
+#   +depth=3 \
+#   +hyper_T=1000 \
+#   +hyper_dt=0.01 \
+#   +loss_plot_ma_window=50
+
+# python unigram/unigram_test2_cart_optim.py -m \
+#   hydra.job.chdir=true \
+#   hydra.sweep.dir=outputs/unigram_test2 \
+#   'hydra.sweep.subdir=unigram_test2_losses_sp${max_steps}_lr${lr}_pt${proposal_type}_per${proposal_exp_rate}_ht${hyper_T}_vs${vocab_size}_cart_optim' \
+#   +folder=artifacts \
+#   +vocab_size=2 \
+#   +max_steps=20000 \
+#   +lr=1e-5 \
+#   '+ps=[0.8,0.2]' \
+#   +proposal_type=exp \
+#   +proposal_exp_rate=0.01,0.1,0.5,1.0,2.0,3.0 \
+#   +hidden_size=128 \
+#   +depth=3 \
+#   +hyper_T=1e12 \
+#   +hyper_dt=0.01 \
+#   +loss_plot_ma_window=50
+
 python unigram/unigram_test2_polar_optim.py -m \
   hydra.job.chdir=true \
   hydra.sweep.dir=outputs/unigram_test2 \
-  'hydra.sweep.subdir=unigram_test2_losses_sp${max_steps}_lr${lr}_pt${proposal_type}_per${proposal_exp_rate}_ht${hyper_T}_vs${vocab_size}_polar_optim' \
+  'hydra.sweep.subdir=unigram_test2_losses_sp${max_steps}_lr${lr}_pt${proposal_type}_per${proposal_exp_rate}_ht${hyper_T}_vs${vocab_size}_vls${val_size}_polar_optim' \
   +folder=artifacts \
-  +vocab_size=2 \
+  +vocab_size=10 \
+  +val_size=90000 \
   +max_steps=20000 \
   +lr=1e-5 \
-  '+ps=[0.8,0.2]' \
+  '+ps=[0.91,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01]' \
   +proposal_type=unif \
   +proposal_exp_rate=1.0 \
   +hidden_size=128 \
@@ -24,12 +93,13 @@ python unigram/unigram_test2_polar_optim.py -m \
 python unigram/unigram_test2_polar_optim.py -m \
   hydra.job.chdir=true \
   hydra.sweep.dir=outputs/unigram_test2 \
-  'hydra.sweep.subdir=unigram_test2_losses_sp${max_steps}_lr${lr}_pt${proposal_type}_per${proposal_exp_rate}_ht${hyper_T}_vs${vocab_size}_polar_optim' \
+  'hydra.sweep.subdir=unigram_test2_losses_sp${max_steps}_lr${lr}_pt${proposal_type}_per${proposal_exp_rate}_ht${hyper_T}_vs${vocab_size}_vls${val_size}_polar_optim' \
   +folder=artifacts \
-  +vocab_size=2 \
+  +vocab_size=10 \
+  +val_size=90000 \
   +max_steps=20000 \
   +lr=1e-5 \
-  '+ps=[0.8,0.2]' \
+  '+ps=[0.91,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01]' \
   +proposal_type=exp \
   +proposal_exp_rate=0.01,0.1,0.5,1.0,2.0,3.0 \
   +hidden_size=128 \
@@ -41,12 +111,13 @@ python unigram/unigram_test2_polar_optim.py -m \
 python unigram/unigram_test2_cart_optim.py -m \
   hydra.job.chdir=true \
   hydra.sweep.dir=outputs/unigram_test2 \
-  'hydra.sweep.subdir=unigram_test2_losses_sp${max_steps}_lr${lr}_pt${proposal_type}_per${proposal_exp_rate}_ht${hyper_T}_vs${vocab_size}_cart_optim' \
+  'hydra.sweep.subdir=unigram_test2_losses_sp${max_steps}_lr${lr}_pt${proposal_type}_per${proposal_exp_rate}_ht${hyper_T}_vs${vocab_size}_vls${val_size}_cart_optim' \
   +folder=artifacts \
-  +vocab_size=2 \
+  +vocab_size=10 \
+  +val_size=90000 \
   +max_steps=20000 \
   +lr=1e-5 \
-  '+ps=[0.8,0.2]' \
+  '+ps=[0.91,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01]' \
   +proposal_type=unif \
   +proposal_exp_rate=1.0 \
   +hidden_size=128 \
@@ -58,12 +129,13 @@ python unigram/unigram_test2_cart_optim.py -m \
 python unigram/unigram_test2_cart_optim.py -m \
   hydra.job.chdir=true \
   hydra.sweep.dir=outputs/unigram_test2 \
-  'hydra.sweep.subdir=unigram_test2_losses_sp${max_steps}_lr${lr}_pt${proposal_type}_per${proposal_exp_rate}_ht${hyper_T}_vs${vocab_size}_cart_optim' \
+  'hydra.sweep.subdir=unigram_test2_losses_sp${max_steps}_lr${lr}_pt${proposal_type}_per${proposal_exp_rate}_ht${hyper_T}_vs${vocab_size}_vls${val_size}_cart_optim' \
   +folder=artifacts \
-  +vocab_size=2 \
+  +vocab_size=10 \
+  +val_size=90000 \
   +max_steps=20000 \
   +lr=1e-5 \
-  '+ps=[0.8,0.2]' \
+  '+ps=[0.91,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01]' \
   +proposal_type=exp \
   +proposal_exp_rate=0.01,0.1,0.5,1.0,2.0,3.0 \
   +hidden_size=128 \
